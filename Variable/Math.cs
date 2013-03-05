7a39afd80f2267dcc4e23883b566bb4b0ee79234
Shourav <shourav9884@gmail.com>
2013-03-05 17:37:24 +0600

    Variable Test 1

---
+++ Variable/Math.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Variable
+{
+    class Math
+    {
+        int x, y;
+
+        int getx()
+        {
+            return x;
+        }
+        int gety()
+        {
+            return y;
+        }
+        public void setxy(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+       public Math(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int Add()
+        {
+            return this.x + this.y;
+        }
+    }
+}
---
+++ Variable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mathObj = new Math(3, 4);
+            System.Console.WriteLine(mathObj.Add());
+            mathObj.setxy(5, 6);
+            System.Console.WriteLine(mathObj.Add());
+            System.Console.ReadLine();
+
+        }
+    }
+}
